061b252d566ddcae9c66342a36621ed20d0943cf
Rizal Iswandy <=>
2023-05-05 23:58:12 +0800

    doodle background

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import { Chat } from "../components/Chat";
 
 function Home() {
   return (
-    <div className="flex flex-col gap-12 overflow-hidden bg-gray-700">
-      <section className="flex flex-col lg:px-[35%] shadow-lg max-h-screen">
-        <h1 className="font-bold text-2xl bg-green-800 text-gray-200 p-4 py-2 h-[8vh] flex items-center">
+    <div className="flex flex-col gap-12 overflow-hidden bg-[url('/img/background.jpg')] bg-contain">
+      <section className="flex flex-col lg:px-[35%] max-h-screen bg-gray-400 bg-opacity-40">
+        <h1 className="font-bold text-2xl bg-green-800 text-gray-200 p-4 py-2 h-[8vh] flex items-center shadow-lg">
           CurHat With A.I:
         </h1>
-        <div className="flex-1">
+        <div className="flex-1 shadow-lg">
           <Chat />
         </div>
       </section>
